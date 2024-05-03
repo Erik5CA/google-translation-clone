aa9d53c5c13ecb61372a9275f114432831235bba
Erik5CA <erik.castillo.anzures@gmail.com>
2024-05-03 12:14:00 -0600

    test to verify the correct translate

---
+++ src/App.test.tsx
@@ -0,0 +1,18 @@
+import { expect, test } from "vitest";
+import { render } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
+import App from "./App";
+
+test("My app works as expected", async () => {
+  const user = userEvent.setup();
+  const app = render(<App />);
+  const textareaFrom = app.getByPlaceholderText("Introducir texto");
+
+  await user.type(textareaFrom, "Hola Mundo");
+  const result = await app.findByDisplayValue(
+    /Hello World/i,
+    {},
+    { timeout: 4000 }
+  );
+  expect(result).toBeTruthy();
+});
