f767cdc375f1c204d6e46928c44e3c204dab3e7b
Erik5CA <erik.castillo.anzures@gmail.com>
2024-05-03 17:18:34 -0600

    updated layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useStore } from "./hooks/useStore";
-import { Container, Row, Col, Button, Stack } from "react-bootstrap";
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Stack,
+  Toast,
+  ToastContainer,
+} from "react-bootstrap";
 import { AUTO_LANGUAGE } from "./constants";
-import { ArrowsIcon, CopyIcon, SpeakerIcon } from "./components/Icons";
+import {
+  ArrowsIcon,
+  CloseIcon,
+  CopyIcon,
+  SpeakerIcon,
+} from "./components/Icons";
 import LanguageSelector from "./components/LanguageSelector";
 import { SectionType } from "./type.d";
 import TextArea from "./components/TextArea";
 import { translate } from "./services/translate";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDebounce } from "./hooks/useDebounce";
 
 function App() {
+  const [show, setShow] = useState(false);
   const handleTranslate = async (textToTranslate: string) => {
     const traduction = await translate({
       fromLanguage,
@@ -37,12 +51,13 @@ function App() {
   const debounceText = useDebounce(text, 300);
 
   useEffect(() => {
-    if (debounceText === "") return;
+    if (debounceText === "" || text === "") return;
     handleTranslate(debounceText);
   }, [debounceText, toLanguage, fromLanguage]);
 
   const handleClipboad = () => {
     navigator.clipboard.writeText(result).catch(() => {});
+    setShow(true);
   };
 
   const handleSpeak = () => {
@@ -51,69 +66,122 @@ function App() {
     speechSynthesis.speak(msg);
   };
 
+  const handleClearFromText = () => {
+    changeText("");
+    // changeResult("");
+  };
   return (
-    <Container fluid>
-      <h1>Google Translate</h1>
+    <Container fluid="lg" className="w-100">
+      <h2 className="m-3 px-3">Google Translate</h2>
 
-      <Row>
+      <ToastContainer position="middle-center" className="mt-4 text-white">
+        <Toast
+          onClose={() => setShow(false)}
+          bg="dark"
+          show={show}
+          delay={2000}
+          autohide
+        >
+          <Toast.Body>Text Copied</Toast.Body>
+        </Toast>
+      </ToastContainer>
+
+      <Container className="mb-5">
         <Col>
-          <Stack gap={2}>
-            <LanguageSelector
-              type={SectionType.From}
-              value={fromLanguage}
-              onChange={changeFromLanguage}
-            />
-            <TextArea
-              type={SectionType.From}
-              value={text}
-              onChange={changeText}
-            />
-          </Stack>
-        </Col>
-
-        <Col xs="auto">
-          <Button
-            variant="outline-primary"
-            disabled={fromLanguage === AUTO_LANGUAGE}
-            onClick={interchangeLanguages}
-          >
-            <ArrowsIcon />
-          </Button>
-        </Col>
-
-        <Col>
-          <Stack gap={2}>
-            <LanguageSelector
-              type={SectionType.To}
-              value={toLanguage}
-              onChange={changeToLanguage}
-            />
-            <div style={{ position: "relative" }}>
-              <TextArea
-                type={SectionType.To}
-                value={result}
-                onChange={changeResult}
-                loading={loading}
+          <div className="d-flex gap-2 mb-2">
+            <Col>
+              <LanguageSelector
+                type={SectionType.From}
+                value={fromLanguage}
+                onChange={changeFromLanguage}
               />
-              <div
-                style={{
-                  position: "absolute",
-                  bottom: 0,
-                  left: 0,
-                  display: "flex",
-                }}
+            </Col>
+            <Col xs="auto">
+              <Button
+                variant="primary"
+                disabled={fromLanguage === AUTO_LANGUAGE}
+                onClick={interchangeLanguages}
               >
-                <Button variant="link" onClick={handleClipboad}>
-                  <CopyIcon />
-                </Button>
-                <Button variant="link" onClick={handleSpeak}>
-                  <SpeakerIcon />
-                </Button>
+                <ArrowsIcon />
+              </Button>
+            </Col>
+            <Col>
+              <LanguageSelector
+                type={SectionType.To}
+                value={toLanguage}
+                onChange={changeToLanguage}
+              />
+            </Col>
+          </div>
+          <Row xs={1} sm={2} md={2} lg={2}>
+            <Col>
+              <div style={{ position: "relative" }}>
+                <TextArea
+                  type={SectionType.From}
+                  value={text}
+                  onChange={changeText}
+                />
+                <div
+                  style={{
+                    position: "absolute",
+                    top: 0,
+                    right: 0,
+                    display: text === "" ? "none" : "flex",
+                  }}
+                >
+                  <Button
+                    variant="link"
+                    title="Borrar Texto"
+                    onClick={handleClearFromText}
+                    disabled={result === ""}
+                  >
+                    <CloseIcon />
+                  </Button>
+                </div>
               </div>
-            </div>
-          </Stack>
+            </Col>
+            <Col>
+              <div style={{ position: "relative" }}>
+                <TextArea
+                  type={SectionType.To}
+                  value={result}
+                  onChange={changeResult}
+                  loading={loading}
+                />
+                <div
+                  style={{
+                    position: "absolute",
+                    bottom: -35,
+                    left: 0,
+                    display: "flex",
+                    backgroundColor: "rgb(33, 37, 41)",
+                    width: "100%",
+                    borderRadius: "0 0 5px 5px",
+                  }}
+                >
+                  <Button
+                    variant="link"
+                    title="Copiar"
+                    onClick={handleClipboad}
+                    disabled={result === ""}
+                  >
+                    <CopyIcon />
+                  </Button>
+
+                  <Button
+                    variant="link"
+                    title="Escuchar"
+                    disabled={result === ""}
+                    onClick={handleSpeak}
+                  >
+                    <SpeakerIcon />
+                  </Button>
+                </div>
+              </div>
+            </Col>
+          </Row>
         </Col>
-      </Row>
+      </Container>
     </Container>
   );
 }
